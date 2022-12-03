4acf98ec9c1cb73061e8565bd2d56db3a2caebcc
Nikita Semchenkov <bachilo.g@mail.ru>
2022-12-03 19:31:45 +0300

    laboratory

---
+++ lab1/Const.py
@@ -0,0 +1,30 @@
+name_image = 'cat'
+image = 'picture/' + name_image + '.png'
+
+ERROR = 0.9
+block_height = 4
+block_width = 4
+compressed_block_height = 2
+compressed_block_width = 2
+alpha = 0.0007
+
+block_col = 1
+block_row = 1
+block_max_col = int(256 / block_width)
+block_max_row = int(256 / block_height)
+first_layer = block_height * block_width * 3
+second_layer = compressed_block_width * compressed_block_height * 3
+number_blocks = int(256 * 256 / (block_height * block_width))
+
+W1 = None
+W2 = None
+h1 = None
+h2 = None
+
+size2 = int(block_max_row * compressed_block_height)
+size1 = int(block_max_col * compressed_block_width)
+
+out_image = None
+middle_image = None
+
+epoch = 0
---
+++ lab3/main.py
@@ -0,0 +1,6 @@
+from NeuralNetwork import NeuralNetwork
+
+if __name__ == '__main__':
+    neural_network = NeuralNetwork()
+    neural_network.start_learning()
+    neural_network.generate_predicted_sequence()
